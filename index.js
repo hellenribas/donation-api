ed73090f52e8712d8975450c321985dde6e0968b
hellenribas <tchwtrabalho@gmail.com>
2024-02-22 12:59:34 -0300

    index changing mongoose

--- index.js
+++ index.js
@@ -17,56 +17,53 @@ app.use(
   })
 );
 
-const client = new MongoClient(process.env.DATABASE_URL, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+// const client = new MongoClient(process.env.DATABASE_URL, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     console.log("run");
+
+//     await client.connect();
+//     console.log("CONNECTED");
+//     // Send a ping to confirm a successful connection
+//     await client.db("Donation-Website").command({ ping: 1 });
+//     console.log(
+//       "Pinged your deployment. You successfully connected to MongoDB!"
+//     );
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on("error", (error) => {
+  console.log(error);
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    console.log("run");
-
-    await client.connect();
-    console.log("CONNECTED");
-    // Send a ping to confirm a successful connection
-    await client.db("Donation-Website").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-// const mongoString = process.env.DATABASE_URL;
-
-// mongoose.connect(mongoString);
-// const database = mongoose.connection;
-
-// database.on("error", (error) => {
-//   console.log(error);
-// });
-
-// database.once("connected", () => {
-//   console.log("Database Connected");
-//   app.use("/", router);
-//   app.use(errors);
-// });
+database.once("connected", () => {
+  console.log("Database Connected");
+  app.use("/", router);
+  app.use(errors);
+});
 
 // Parse.initialize(
 //   process.env.PARSE_APPLICATION_ID,
 //   process.env.PARSE_JAVASCRIPT_KEY
 // );
 // Parse.serverURL = process.env.PARSE_HOST_URL;
-
-app.use("/", router);
-app.use(errors);
 
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
