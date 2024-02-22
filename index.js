7ee7cf3a6de777268103d492bd176f595ebefcec
hellenribas <tchwtrabalho@gmail.com>
2024-02-22 12:11:48 -0300

    config deploy

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const router = require("./routes/routes");
 const { errors } = require("./middlewares/messages");
 const cors = require("cors");
@@ -16,20 +17,47 @@ app.use(
   })
 );
 
-const mongoString = process.env.DATABASE_URL;
-
-mongoose.connect(mongoString);
-const database = mongoose.connection;
-
-database.on("error", (error) => {
-  console.log(error);
+const client = new MongoClient(process.env.DATABASE_URL, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-database.once("connected", () => {
-  console.log("Database Connected");
-  app.use("/", router);
-  app.use(errors);
-});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    console.log("run");
+
+    await client.connect();
+    console.log("CONNECTED");
+    // Send a ping to confirm a successful connection
+    await client.db("Donation-Website").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// const mongoString = process.env.DATABASE_URL;
+
+// mongoose.connect(mongoString);
+// const database = mongoose.connection;
+
+// database.on("error", (error) => {
+//   console.log(error);
+// });
+
+// database.once("connected", () => {
+//   console.log("Database Connected");
+//   app.use("/", router);
+//   app.use(errors);
+// });
 
 // Parse.initialize(
 //   process.env.PARSE_APPLICATION_ID,
